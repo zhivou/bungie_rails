6655cb6df7101fbd9792b9adb7baa2e3c4a2dceb
Dmitrii Skrylev <dima.skrylev@plansource.com>
2019-07-16 11:17:29 -0600

    Added raw sql to query a propper ids

--- app/controllers/api/destiny_inventory_item_definition_controller.rb
+++ app/controllers/api/destiny_inventory_item_definition_controller.rb
@@ -5,6 +5,9 @@ class Api::DestinyInventoryItemDefinitionController < ApplicationController
   end
 
   def destiny_inventory_item
-    render json: DestinyInventoryItemDefinition.find(params[:item_id]).json
+    render json: DestinyInventoryItemDefinition.find_by_sql("
+      SELECT json FROM DestinyInventoryItemDefinition
+      WHERE json LIKE '%#{params[:item_id]}%'
+    ")
   end
 end
